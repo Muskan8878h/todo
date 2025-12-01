2fac71add35af240373e5875a98e6d18d51ba94e
Muskan8878h <muskan8878h@gmail.com>
2025-12-01 20:38:52 +0530

    .

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Express app setup
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => { console.log('Connected to MongoDB'); })
+    .catch(() => { console.log('Error connecting to MongoDB:', err); });
+
+
+const todo=mongoose.model('Todo',new mongoose.Schema({
+    text:String
+}));    
+
+
+// Basic route
+app.get('/todos', async(req, res) => {
+    res.json(await todo.find());
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
