256798c61d0fbaad9c8848c871e52d1fb7dae41c
Muskan8878h <muskan8878h@gmail.com>
2025-12-02 21:04:43 +0530

    .

--- server.js
+++ server.js
@@ -14,29 +14,49 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => { console.log('Connected to MongoDB'); })
-    .catch((err) => console.log('Error connecting to MongoDB:', err));
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log('Error connecting to MongoDB:', err));
 
 
-const todo=mongoose.model('Todo',new mongoose.Schema({
-    text:String
-}));    
+// MODEL (correct name)
+const Todo = mongoose.model('Todo', new mongoose.Schema({
+    text: { type: String, required: true }
+}));
 
 
-// Basic route
-app.get('/todos', async(req, res) => {
-    res.json(await todo.find());
+// --------------------------------------
+// ROUTES
+// --------------------------------------
+
+// GET all todos
+app.get('/todos', async (req, res) => {
+    try {
+        const todos = await Todo.find();
+        res.json(todos);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
-app.post('/todos', async (req,res) => {
-    const todo = await Todo.create({ text : req.body.text });
-    res.json(todo);
-})
+// POST create new todo
+app.post('/todos', async (req, res) => {
+    try {
+        const todo = await Todo.create({ text: req.body.text });
+        res.json(todo);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
-app.delete("/todo/:id", async (req, res) => {
-    await Todo.findByIdAndDelete(req.params.id);
-    res.json({message : "Deleted"})
-})
+// DELETE todo
+app.delete('/todos/:id', async (req, res) => {
+    try {
+        await Todo.findByIdAndDelete(req.params.id);
+        res.json({ message: "Deleted" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
 
 // Start the server
